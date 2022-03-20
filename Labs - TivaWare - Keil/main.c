4143f04a1027afa552e922e56c4a441e277e7fe8
Mazen <mazenghaleb@outlook.com>
2022-03-20 16:15:48 +0200

    Made code only for Red toggle as required

--- Labs - TivaWare - Keil/main.c
+++ Labs - TivaWare - Keil/main.c
@@ -1,15 +1,12 @@
 #include "Init.h"
 
 void toggle_red(void);
-void toggle_blue(void);
 static void blink_red(void *pvParameters);
-static void blink_blue(void *pvParameters);
 
 int main() 
 { 
   PortFInit();
 	xTaskCreate(blink_red,"Blink Red", configMINIMAL_STACK_SIZE,NULL , 1, NULL);
-	xTaskCreate(blink_blue,"Blink Blue", configMINIMAL_STACK_SIZE,NULL , 1, NULL);
 	vTaskStartScheduler();
 }
 
@@ -21,24 +18,9 @@ void toggle_red()
     GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1,GPIO_PIN_1);
 }
 
-void toggle_blue()
-{
-  if(GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_2) & (1<<2))
-    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2,0);
-  else
-    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2,GPIO_PIN_2);
-}
-
 void blink_red(void *pvParameters){
   for(;;){
   toggle_red();
-  vTaskDelay(2000/ portTICK_RATE_MS);
-  }
-}
-
-void blink_blue(void *pvParameters){
-  for(;;){
-  toggle_blue();
-  vTaskDelay(1000 / portTICK_RATE_MS);
+  vTaskDelay(1000/ portTICK_RATE_MS);
   }
 }
