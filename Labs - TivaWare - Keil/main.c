de8525274ba4af9fe5f34e2e17b72d6625aa02cf
Mazen <mazenghaleb@outlook.com>
2022-04-14 10:00:16 +0200

    Lab 5

--- Labs - TivaWare - Keil/main.c
+++ Labs - TivaWare - Keil/main.c
@@ -1,5 +1,6 @@
 #include "Init.h"
 
+void vApplicationIdleHook();
 void toggle_red(void);
 static void blink_red(void *pvParameters);
 static void blink_blue(void *pvParameters);
@@ -9,27 +10,24 @@ int main()
 { 
   PortFInit();
 	xTaskCreate(blink_red,"Blink Red", configMINIMAL_STACK_SIZE,NULL , 1, NULL);
-	xTaskCreate(blink_blue,"Blink Blue", configMINIMAL_STACK_SIZE,NULL , 1, NULL);
 	xTaskCreate(blink_green,"Blink Green", configMINIMAL_STACK_SIZE,NULL , 1, NULL);
 	
 	vTaskStartScheduler();
 }
+
 void blink_red(void *pvParameters){
   for(;;){
   toggle_red();
   vTaskDelay(1000/ portTICK_RATE_MS);
   }
 }
-void blink_blue(void *pvParameters){
-  for(;;){
-  toggle_blue();
-  vTaskDelay(2000/ portTICK_RATE_MS);
-  }
-}
 void blink_green(void *pvParameters){
   for(;;){
   toggle_green();
-  vTaskDelay(3000/ portTICK_RATE_MS);
+  vTaskDelay(2000/ portTICK_RATE_MS);
   }
 }
 
+void vApplicationIdleHook(){
+	toggle_blue();
+}
