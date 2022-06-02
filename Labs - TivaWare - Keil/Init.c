9c8a7aa236d99d3d2037ca2f526bff4132fe4dd0
Mohamed Mokhtar <mokhtar.mohammed.red@gmail.com>
2022-06-02 22:29:41 +0200

    Added comments

--- Labs - TivaWare - Keil/Init.c
+++ Labs - TivaWare - Keil/Init.c
@@ -1,9 +1,22 @@
 #include "Init.h"
 
+/*
+ * Description:
+ * Converts the number of milliseconds to ticks (assuming 16Mhz clock speed)
+ */
 uint32 CalcTicks(uint32 milliseconds){
 return ((milliseconds) * (16 * 1000))-1 ;
 }	
 
+/*
+ * Description:
+ * Intializes Port F pins. The setup is as follows:
+ *   - Pin 0: Input (switch)
+ *   - Pin 1: Output (LED)
+ *   - Pin 2: Output (LED)
+ *   - Pin 3: Output (LED)
+ *   - Pin 4: Input (switch) 
+ */
 void PortFInit(void) {
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
   while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)) {}
@@ -14,6 +27,13 @@ void PortFInit(void) {
   GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU); 
   GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);    
 }
+
+/*
+ * Description:
+ * Intializes Port A pins. The setup is as follows:
+ *   - Pin 0: UART0 RX
+ *   - Pin 1: UART0 TX
+ */
 void PortAInit(void) {
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
   while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA)) {}
@@ -22,12 +42,22 @@ void PortAInit(void) {
 	GPIOPinTypeUART(GPIO_PORTA_BASE , GPIO_PIN_0 | GPIO_PIN_1);
 }
 
+/*
+ * Description:
+ * Intializes Port E pins. The setup is as follows:
+ *   - Pin 3: ADC (tempreature sensor)
+ */
 void PortEInit(void) {
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
   while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE)) {}
   GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_3);
 }
 
+/*
+ * Description:
+ * Intializes Port E pins by calling PortEInit() then configures the pin as ADC0.
+ *   - 
+ */
 void ADCInit(void){
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
 	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_ADC0)){}
@@ -40,6 +70,10 @@ void ADCInit(void){
   //while(!ADCIntStatus(ADC0_BASE, 3, false)){}
 }
 
+/*
+ * Description:
+ * Intializes the UART0 peripheral and enables it with a baud rate of 9600.
+ */
 void UART0Init(void){
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
 	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_UART0)){}
@@ -50,7 +84,10 @@ void UART0Init(void){
 	UART_CONFIG_PAR_NONE));
 	UARTEnable(UART0_BASE);
 }
-
+/*
+ * Description:
+ * Intializes the switches and enables the interrupts with a priority of 7 (to not interfere with FreeRTOS).
+ */
 void SwitchInterruptInit(void){
   GPIOIntDisable(GPIO_PORTF_BASE,GPIO_PIN_4|GPIO_PIN_0);        // Sandwich
   GPIOIntClear(GPIO_PORTF_BASE,GPIO_PIN_4|GPIO_PIN_0);          // Clear any existing interrupts
